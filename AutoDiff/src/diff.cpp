e78f6ab1e9b5eef9ce83538550a8986fff4eccd2
morrev <70282228+morrev@users.noreply.github.com>
2020-11-12 14:31:14 -0500

    Camel-casing 'autodiff' as per C++ style convention

--- AutoDiff/src/diff.cpp
+++ AutoDiff/src/diff.cpp
@@ -1,97 +1,97 @@
 #include "AutoDiff.h"
 #include <math.h>
 
-autodiff sin(autodiff input)
+AutoDiff sin(AutoDiff input)
 {
-    autodiff output;
+    AutoDiff output;
     output.val = sin(input.val);
     output.dv = cos(input.val) * input.dv
     return output;
 }
 
-autodiff cos(autodiff input)
+AutoDiff cos(AutoDiff input)
 {   
-    autodiff output;
+    AutoDiff output;
     output.val = cos(input.val);
     output.dv = -sin(input.val) * input.dv
     return output;
 }
 
-autodiff tan(autodiff input)
+AutoDiff tan(AutoDiff input)
 {
-    autodiff output;
+    AutoDiff output;
     output.val = tan(input.val);
     output.dv = (1/pow(cos(input.val), 2)) * input.dv;
     return output;
 }
 
-autodiff arcsin(autodiff input)
+AutoDiff arcsin(AutoDiff input)
 {
-    autodiff output;
+    AutoDiff output;
     output.val = asin(input.val);
     output.dv = (1/sqrt(1 - pow(input.val, 2))) * input.dv;
     return output;
 }
 
-autodiff arccos(autodiff input)
+AutoDiff arccos(AutoDiff input)
 {
-    autodiff output;
+    AutoDiff output;
     output.val = acos(input.val);
     output.dv = -(1/sqrt(1 - pow(input.val, 2))) * input.dv;
     return output;
 }
 
-autodiff arctan(autodiff input)
+AutoDiff arctan(AutoDiff input)
 {
-    autodiff output;
+    AutoDiff output;
     output.val = atan(input.val);
     output.dv = (1/(1+pow(input.val, 2))) * input.dv;
     return output;
 }
 
-autodiff exp(autodiff input)
+AutoDiff exp(AutoDiff input)
 {
-    autodiff output;
+    AutoDiff output;
     output.val = exp(input.val);
     output.dv = exp(input.val) * input.dv;
     return output;
 }
 
-autodiff log(autodiff input)
+AutoDiff log(AutoDiff input)
 {
-    autodiff output;
+    AutoDiff output;
     output.val = log(input.val);
     output.dv = (1/input.val) * input.dv;
     return output;
 }
 
-autodiff pow(autodiff input, double exponent)
+AutoDiff pow(AutoDiff input, double exponent)
 {
-    autodiff output;
+    AutoDiff output;
     output.val = pow(input.val, exponent);
     output.dv = exponent * pow(input.val, exponent - 1) * input.dv;
     return output;
 }
 
-autodiff sinh(autodiff input)
+AutoDiff sinh(AutoDiff input)
 {
-    autodiff output;
+    AutoDiff output;
     output.val = sinh(input.val);
     output.dv = cosh(input.val) * input.dv;
     return output;
 }
 
-autodiff cosh(autodiff input)
+AutoDiff cosh(AutoDiff input)
 {
-    autodiff output;
+    AutoDiff output;
     output.val = cosh(input.val);
     output.dv = sinh(input.va) * input.dv;
     return output;
 }
 
-autodiff tanh(autodiff input)
+AutoDiff tanh(AutoDiff input)
 {
-    autodiff output;
+    AutoDiff output;
     output.val = tanh(input.val);
     output.dv = (1/pow(cosh(input.val), 2)) * input.dv;
     return output;
