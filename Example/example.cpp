e36b0230623d164ffcf8ee5a71bdff7e2056f87b
morrev <70282228+morrev@users.noreply.github.com>
2020-12-10 16:54:05 -0500

    Minor changes to example.cpp

--- Example/example.cpp
+++ Example/example.cpp
@@ -21,7 +21,12 @@ vector<double> der(function<vector<AutoDiff> (vector<AutoDiff>)> func, vector<Au
 vector<AutoDiff> F(vector<AutoDiff> vars){
     AutoDiff x = vars[0];
     AutoDiff y = vars[1];
-    return {y*sin(x) + exp(x)*cos(y), 2*x + 3*y};
+    return {y*sin(x) + exp(x)*cos(y), 2*x + 3*y, (y*sin(x) + exp(x)*cos(y))*(2*x+3*y)};
+}
+
+vector<AutoDiff> F2(vector<AutoDiff> vars){
+    AutoDiff x = vars[0];
+    return {sin(pow(x,3))};
 }
 
 int main(){
@@ -34,5 +39,14 @@ int main(){
     vector<double> dF = der(myFunction, vars, wrt = 0);
     cout << "Derivative of f1: " << dF[0] << endl;
     cout << "Derivative of f2: " << dF[1] << endl;
+    cout << "Derivative of f3: " << dF[2] << endl;
+
+
+    AutoDiff x1(2,0);
+    vector<AutoDiff> vars2 = {x1};
+    function<vector<AutoDiff>(vector<AutoDiff>)> myFunction2 = F2;
+    vector<double> dF2 = der(myFunction2, vars2, wrt=0);
+    cout << "Derivative of f1: " << dF2[0] << endl;
+    
     return 0;
 }
