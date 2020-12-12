20ab7e6d60900d5625bcfc79f9ea766fb0b5d984
Wenlin Gong <wenlingong@Wenlins-MacBook-Pro.local>
2020-12-12 10:07:48 -0500

    Add back googletest

--- Dual/build/core/tests/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/wenlingong/Documents/GitHub/Classes/cs107/cs107-FinalProject/Dual/core/tests
-# Build directory: /Users/wenlingong/Documents/GitHub/Classes/cs107/cs107-FinalProject/Dual/build/core/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(DUALTest.Unit.Test "/Users/wenlingong/Documents/GitHub/Classes/cs107/cs107-FinalProject/Dual/build/bin/DUALTest.Unit.Test")
-set_tests_properties(DUALTest.Unit.Test PROPERTIES  _BACKTRACE_TRIPLES "/Users/wenlingong/Documents/GitHub/Classes/cs107/cs107-FinalProject/Dual/core/tests/CMakeLists.txt;27;add_test;/Users/wenlingong/Documents/GitHub/Classes/cs107/cs107-FinalProject/Dual/core/tests/CMakeLists.txt;0;")
-subdirs("src")
